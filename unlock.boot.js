74226af10adc6f4155681d05ace0c52bee322efc
Alex Meyer <alexwreyem@gmail.com>
2023-09-08 00:33:10 -0400

    fix backlight spazz with wakeOnTouch

--- unlock.boot.js
+++ unlock.boot.js
@@ -1,4 +1,5 @@
 // this script disables the auto-lock, so the screen can be used without having to unlock the watch
+// I keep going back and forth on whether or not I actually like this setup
 {
   // turns the backlight on and resets the activity timer
   let handler = function() {
@@ -10,15 +11,17 @@
   Bangle.on('swipe', handler);
 
   // handle button presses
-  setWatch(handler, BTN1, { repeat:true, edge:'rising', debounce:0 });
+  setWatch(handler, BTN1, { repeat:true, edge:'rising' });
 
-  // disable (max out) lock timeout, and disable wakeOnBTN1.
-  // wakeOnBTN1 is the important one to disable, otherwise the first press gets
-  // consumed and doesn't do anything. at least one of the other wakeOns needs
-  // to be enabled, otherwise BTN1 is forced back on.
+  // disable (max out) lock timeout, and disable some wakeOns.
+  // wakeOnBTN1 is important to disable, otherwise the first press gets
+  // consumed and doesn't do anything. also wakeOnTouch seems to spazz
+  // out the backlight if it's left on.
   Bangle.setOptions({
     lockTimeout: 1000 * 60 * 60 * 24 * 365,
     wakeOnBTN1: false,
-    wakeOnTouch: true,
+    wakeOnTouch: false,
+    wakeOnFaceUp: false,
+    //wakeOnTwist: false, // leaving this one un-forced in case I want it on
   });
 }
